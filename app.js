fd15f716720edc3da287dd35b3892406a122f90a
saurin thakkar <saurin.thakkar13@gmail.com>
2021-06-07 00:45:25 +0530

    Added input event listener

--- app.js
+++ app.js
@@ -5,10 +5,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
 btnTranslate.addEventListener("click",clickEventHandler);
 
+
+
+// console.log(btnTranslate);
+
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
 function clickEventHandler(){
     console.log("clicked");
+    console.log("input",txtInput.value);
 }
 
-var txtInput = document.querySelector("")
-
-console.log(btnTranslate);
