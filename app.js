af27f54389b80dfe78313570a7050a9347a3b323
saurin thakkar <saurin.thakkar13@gmail.com>
2021-06-13 00:30:05 +0530

    feat : added fetch call

--- app.js
+++ app.js
@@ -7,14 +7,32 @@ btnTranslate.addEventListener("click",clickEventHandler);
 
 var outputDiv = document.querySelector("#output");
 
-console.log(outputDiv)
+//console.log(outputDiv)
 // console.log(btnTranslate);
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?text=" + text;
+}
+
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+//console.log(txtInput);
+function errorHandler(error){
+    console.log("error occured",error)
+
+}
+
 
 function clickEventHandler(){
-    console.log("clicked");
-    outputDiv.innerText="abbbsdahsd " + txtInput.value;
+    //console.log("clicked");
+    //outputDiv.innerText="abbbsdahsd " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then (response => response.json())
+    .then (json => console.log(json.contents.translated))
+    .catch(errorHandler)
+
 }
 
