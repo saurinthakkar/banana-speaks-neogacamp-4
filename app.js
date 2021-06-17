68d3f2dea7c6e96b73e1e0688ef362fe3b6bf51f
saurin thakkar <saurin.thakkar13@gmail.com>
2021-06-18 01:24:58 +0530

    feat: added text output in outdiv

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var outputDiv = document.querySelector("#output");
 //console.log(outputDiv)
 // console.log(btnTranslate);
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
     return serverURL + "?text=" + text;
@@ -27,11 +27,15 @@ function errorHandler(error){
 function clickEventHandler(){
     //console.log("clicked");
     //outputDiv.innerText="abbbsdahsd " + txtInput.value;
-    var inputText = txtInput.value;
+    var inputText = txtInput.value; // taking input
+
+    //calling server for processing
 
     fetch(getTranslationURL(inputText))
     .then (response => response.json())
-    .then (json => console.log(json.contents.translated))
+    .then (json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;})
     .catch(errorHandler)
 
 }
